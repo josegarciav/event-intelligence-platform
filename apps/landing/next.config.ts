dfb951ca10b077c7f5b37a309e7160583361fe23
Jose Garcia Ventura <57261516+josegarciav@users.noreply.github.com>
2026-02-11 00:32:38 +0100

    fix: landing page problem

--- apps/landing/next.config.ts
+++ apps/landing/next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   basePath: "/event-intelligence-platform",
+  assetPrefix: "/event-intelligence-platform/",
   reactStrictMode: true,
   compiler: {
     removeConsole: process.env.NODE_ENV === "production",
